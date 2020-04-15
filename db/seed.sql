012a2d408e8f5b79079fa4da54c976fb812d89db
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-04-15 18:12:28 -0400

    schema and seed complete

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Garlic Burger');
+INSERT INTO burgers (name) VALUES ('Mozarella Burger');
+INSERT INTO burgers (name) VALUES ('Divine Burger');
+INSERT INTO burgers (name) VALUES ('Regular Cheese Burger');
