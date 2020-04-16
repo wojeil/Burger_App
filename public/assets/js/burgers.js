49413de662719821f683072cfceea536cd627066
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-04-15 21:12:54 -0400

    wokring on errors

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -27,7 +27,7 @@ $(function() {
   
       var newBurger = {
         name: $("#bu").val().trim(),
-        eat: $("[name=eat]:checked").val().trim()
+        eat: 0
       };
   
       // Send the POST request.
@@ -37,21 +37,6 @@ $(function() {
       }).then(
         function() {
           console.log("created new burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".delete-burger").on("click", function(event) {
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/burger/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted burger", id);
           // Reload the page to get the updated list
           location.reload();
         }
